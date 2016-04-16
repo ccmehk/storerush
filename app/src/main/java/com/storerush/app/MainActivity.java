45224fede1d32d4a3d47b4f82041eff700afecfa
Nathan <ccmehk@gmail.com>
2016-04-16 13:42:59 +0800

    Modify action bar

--- app/src/main/java/com/storerush/app/MainActivity.java
+++ app/src/main/java/com/storerush/app/MainActivity.java
@@ -1,5 +1,8 @@
 package com.storerush.app;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar bar = getSupportActionBar();
+        bar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        bar.setCustomView(R.layout.actionbar);
+        bar.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
     }
 }
